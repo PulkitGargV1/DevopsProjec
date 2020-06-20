0514dbf7942f8d18565b11fa318d49d17ee1b01f
PulkitGargV1 <63607257+PulkitGargV1@users.noreply.github.com>
2020-06-20 15:54:54 +0100

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,18 +3,15 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
 
 dockerpath="gargpulkit/flask_app"
 
-# Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login &&\
     docker image tag flask_app $dockerpath
-# Step 3:
+
 # Push image to a docker repository
 
 docker image push $dockerpath 
