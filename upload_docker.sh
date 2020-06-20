8ca0d9bfe2c1144abf91fc6a2d879313f44e04ae
Pulkit Garg <garg@Pulkits-MacBook-Pro.local>
2020-06-20 14:56:55 +0100

    first commit

---
+++ upload_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+
+dockerpath="gargpulkit/flask_app"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag flask_app $dockerpath
+# Step 3:
+# Push image to a docker repository
+
+docker image push $dockerpath 
+
